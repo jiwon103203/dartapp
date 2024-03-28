f7441ace8490738a30355f78876ecbfea6a4030b
김지원 <127197114+jiwon103203@users.noreply.github.com>
2024-03-28 21:12:52 +0900

    Update eda.py

--- eda.py
+++ eda.py
@@ -51,7 +51,8 @@ with col4:
 graph_type=st.selectbox("그래프 종류",["막대그래프","선그래프"],key="graph_type")
 submit_corp=st.button("Submit",key="submit_corp")
 
-dart_fss.set_api_key()
+print("api key는 다음과 같음",api_key)
+dart_fss.set_api_key(api_key=api_key)
 corp_code=[]
 if submit_corp:
     df=pd.DataFrame(dart_fss.api.filings.get_corp_code())
